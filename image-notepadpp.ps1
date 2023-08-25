fb9561a46b2e3f79e03d0b9e1b3b4f7b3db042f1
Love Håkansson <love.hakansson@edrmedeso.com>
2023-08-25 19:40:49 +0200

    Add files via upload

---
+++ image-notepadpp.ps1
@@ -0,0 +1,45 @@
+<#
+Script for setting up the VDI host image
+Love Håkansson 2023-08-23
+#>
+
+# Create a temp directory on the VDI host
+New-Item -ItemType directory -Path c:/temp
+
+# Downloading and installing AzCopy
+Invoke-WebRequest -Uri https://aka.ms/downloadazcopy-v10-windows -OutFile c:\temp\AzCopy.zip
+Expand-Archive c:\temp\AzCopy.zip c:\temp\AzCopy
+Get-ChildItem c:\temp\AzCopy\*\azcopy.exe | Move-Item -Destination C:\temp\AzCopy.exe
+
+# Downloading some software from the storage account
+Start-Process -FilePath C:\temp\AzCopy.exe -ArgumentList @('login', '--identity') -Wait
+Start-Process -FilePath C:\temp\AzCopy.exe -ArgumentList @('cp', 'https://strgvdi.blob.core.windows.net/image-software/npp.8.5.6.Installer.x64.exe', 'c:\temp') -Wait
+
+# Installing Notepad++
+Start-Process -FilePath c:\temp\npp.8.5.6.Installer.x64.exe -ArgumentList /S -Wait
+<#
+# Mounting the ANSYS .iso files
+$iso1 = Mount-DiskImage -ImagePath c:/temp/ANSYS2023R2_WINX64_DISK1.iso -StorageType ISO -PassThru
+$iso2 = Mount-DiskImage -ImagePath c:/temp/ANSYS2023R2_WINX64_DISK2.iso -StorageType ISO -PassThru
+$iso3 = Mount-DiskImage -ImagePath c:/temp/ANSYS2023R2_WINX64_DISK3.iso -StorageType ISO -PassThru
+$drive1 = ($iso1 | Get-Volume).DriveLetter
+$drive2 = ($iso2 | Get-Volume).DriveLetter
+$drive3 = ($iso3 | Get-Volume).DriveLetter
+
+# Building the launch command line
+$dir2 = $drive2 + ':\\'
+$dir3 = $drive3 + ':\\'
+$cmd = $drive1 + ':\\setup.exe'
+$argList = @('-silent', '-fluent', '-spaceclaim', '-optislang', '-media_dir2', $dir2, '-media_dir3', $dir3, '-licserverinfo', '2325:1055:rescale-license')
+
+# Installing ANSYS
+Start-Process -FilePath $cmd -ArgumentList $argList -Wait
+
+# Unmounting the .iso files
+Dismount-DiskImage -ImagePath c:\temp\ANSYS2023R2_WINX64_DISK1.iso
+Dismount-DiskImage -ImagePath c:\temp\ANSYS2023R2_WINX64_DISK2.iso
+Dismount-DiskImage -ImagePath c:\temp\ANSYS2023R2_WINX64_DISK3.iso
+
+# Removing the entire c:\temp folder
+Remove-Item c:\temp\ -Recurse
+#>
