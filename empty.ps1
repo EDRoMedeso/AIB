4c4109bb376fe2a463a1ace59c5b08836d726392
Love Håkansson <love.hakansson@edrmedeso.com>
2023-08-27 10:44:55 +0200

    Update empty.ps1

--- empty.ps1
+++ empty.ps1
@@ -1,1 +1,8 @@
 Get-ChildItem -Path c:\
+# Create a temp directory on the VDI host
+New-Item -ItemType directory -Path c:\temp
+
+# Downloading and installing AzCopy
+Invoke-WebRequest -Uri https://aka.ms/downloadazcopy-v10-windows -OutFile c:\temp\AzCopy.zip
+Expand-Archive c:\temp\AzCopy.zip c:\temp\AzCopy
+Get-ChildItem c:\temp\AzCopy\*\azcopy.exe | Move-Item -Destination C:\temp\AzCopy.exe
