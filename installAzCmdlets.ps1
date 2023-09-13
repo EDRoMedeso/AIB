981fc6c6ee96ace306f7dca3db78276437ed127c
Love Håkansson <love.hakansson@edrmedeso.com>
2023-09-13 23:17:00 +0200

    Update installAzCmdlets.ps1

--- installAzCmdlets.ps1
+++ installAzCmdlets.ps1
@@ -1,6 +1,6 @@
 # This script installs the Az PowerShell module
 
-Set-ExecutionPolicy -Scope LocalMachine -ExecutionPolicy Bypass -Force
+# Set-ExecutionPolicy -Scope LocalMachine -ExecutionPolicy Bypass -Force
 Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force
 Set-PSRepository -Name PSGallery -InstallationPolicy Trusted
 Install-Module -Name Az
