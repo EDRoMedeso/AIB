2c2b4e1a5a99526583fe2cf2fdd1f77bfbceef5e
Love Håkansson <love.hakansson@edrmedeso.com>
2023-09-14 08:30:11 +0200

    Update vdi-prodconf2023r2.ps1

--- vdi-prodconf2023r2.ps1
+++ vdi-prodconf2023r2.ps1
@@ -3,17 +3,19 @@ Script for running ProductConfig.exe on the VDI hosts during build
 Love Håkansson 2023-09-13
 #>
 
-# Fetching the password file for the 'imageInstall' account on \\spark
+# Fetching the password file for the 'imageInstall' account on \\spark and connecting to the share
 Connect-AzAccount -Identity
 $username = 'spark.internal.edrm\imageinstall'
 $password = (Get-AzKeyVaultSecret -VaultName kvedrm -Name imageBuilderPassWordSpark -AsPlainText) | ConvertTo-SecureString -AsPlainText -Force
-# $spassword = ConvertTo-SecureString $password -AsPlainText -Force
-$credential = new-object -typename System.Management.Automation.PSCredential -argumentlist $username, $password
+$credential = New-Object -Typename System.Management.Automation.PSCredential -ArgumentList $username, $password
+New-PSDrive -Name "Z" -PSProvider "FileSystem" -Root "\\spark.internal.edrm\ansys inc" -Credential $credential
 
+# Building the command line for Ansys ProductConfig
 $argList = @('-silent', '-config', '-soundsas', '-discovery', '-spaceclaim', '-icepak', '-cfx', '-chemkinpro', '-ensight', '-fensapice', '-fluent', '-forte',
 '-icemcfd', '-mfl', '-polyflow', '-reactionwb', '-turbogrid', '-speos', '-speoshpc', '-optislang', '-additive', '-aqwa', '-autodyn', '-ansyscust',
 '-lsdyna', '-mechapdl', '-motion', '-sherlock')
-New-PSDrive -Name "Z" -PSProvider "FileSystem" -Root "\\spark.internal.edrm\ansys inc" -Credential $credential
+
+# Installing prerequisites, .NET, and configuring products
 Start-Process -FilePath "\\spark.internal.edrm\ansys inc\v232\ProductConfig.exe" -ArgumentList @('-silent', '-prereqs') -Wait
 Start-Process -FilePath "\\spark.internal.edrm\ansys inc\v232\ProductConfig.exe" -ArgumentList @('-silent', '-caspol') -Wait
 Start-Process -FilePath "\\spark.internal.edrm\ansys inc\v232\ProductConfig.exe" -ArgumentList $argList -Wait
