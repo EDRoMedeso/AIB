2b9c20dda7271d525696af14ca5bda4607da6b88
Love Håkansson <love.hakansson@edrmedeso.com>
2023-09-20 10:08:44 +0200

    Update snagit2023.ps1

--- snagit2023.ps1
+++ snagit2023.ps1
@@ -6,4 +6,4 @@ Love Håkansson 2023-09-20
 Invoke-WebRequest -Uri https://strgedrm.blob.core.windows.net/software/snagit.mst -OutFile c:\edrm\snagit.mst
 Invoke-WebRequest -Uri https://strgedrm.blob.core.windows.net/software/snagit.msi -OutFile c:\edrm\snagit.msi
 
-Start-Process -FilePath msiexec -ArgumentList @('/i', 'c:\\edrm\\snagit.msi', 'TRANSFORMS="c:\\edrm\\snagit.mst") -Wait
+Start-Process -FilePath msiexec -ArgumentList @('/i', 'c:\\edrm\\snagit.msi', 'TRANSFORMS="c:\\edrm\\snagit.mst"', '/qn') -Wait
