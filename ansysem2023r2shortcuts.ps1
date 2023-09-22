4b0c840174e9b5f77315d8ac8648bba9664e85bf
Love Håkansson <love.hakansson@edrmedeso.com>
2023-09-22 23:20:35 +0200

    Update ansysem2023r2shortcuts.ps1

--- ansysem2023r2shortcuts.ps1
+++ ansysem2023r2shortcuts.ps1
@@ -3,7 +3,7 @@ Script to copy the Start menu items for Ansys Electronics 2023R2
 Love Håkansson 2023-09-22
 #>
 
-Invoke-WebRequest -Uri https://strgedrm.blob.core.windows.net/software/ansysem.zip\ -OutFile c:\edrm\ansysem.zip
+Invoke-WebRequest -Uri https://strgedrm.blob.core.windows.net/software/ansysem.zip -OutFile c:\edrm\ansysem.zip
 Expand-Archive -Path c:\edrm\ansysem.zip -DestinationPath 'C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Ansys EM Suite 2023 R2' -Force
 
 [Environment]::SetEnvironmentVariable('ANSYSEM_ROOT232', '\\spark\ansysem\v232\win64', 'Machine')
