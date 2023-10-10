5fc4924ae4fcd0a3b6d2764cadfd72534942527b
Love Håkansson <love.hakansson@edrmedeso.com>
2023-10-10 15:32:48 +0200

    Create vscode.ps1

---
+++ vscode.ps1
@@ -0,0 +1,7 @@
+<#
+Script to install VS Code
+Love Håkansson 2023-10-10
+#>
+
+Invoke-WebRequest -Uri https://strgedrm.blob.core.windows.net/software/VSCodeSetup-x64-1.83.0.exe -OutFile c:\edrm\VSCodeSetup-x64-1.83.0.exe
+Start-Process -FilePath VSCodeSetup-x64-1.83.0.exe -ArgumentList @('/SP-', '/VERYSILENT', '/NORESTART', '/NOCANCEL', '/MERGETASKS=!runcode') -Wait
