858b3e27f4d25c7d27e51d918019694eea10346f
Love Håkansson <love.hakansson@edrmedeso.com>
2023-11-04 16:04:46 +0100

    Update vlc.ps1

--- vlc.ps1
+++ vlc.ps1
@@ -4,4 +4,4 @@ Love Håkansson 2023-11-04
 #>
 
 Invoke-WebRequest -Uri https://strgedrm.blob.core.windows.net/software/vlc-3.0.20-win64.exe -OutFile c:\edrm\vlc-3.0.20-win64.exe
-Start-Process -FilePath c:\edrm\vlc-3.0.20-win64.exe -ArgumentList @('/L=1033', '/S') -Wait
+Start-Process -FilePath c:\edrm\vlc-3.0.20-win64.exe -ArgumentList @('/L=1033', '/S') -Waitgg
