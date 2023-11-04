6f3b1e2f6bacbcf1e63e1f764dd025f1a0b7ca4c
Love Håkansson <love.hakansson@edrmedeso.com>
2023-11-04 16:00:12 +0100

    Update vlc.ps1

--- vlc.ps1
+++ vlc.ps1
@@ -1,7 +1,7 @@
 <#
 Script to install VLC Media Player
-Love Håkansson 2023-10-09
+Love Håkansson 2023-11-04
 #>
 
-Invoke-WebRequest -Uri https://strgedrm.blob.core.windows.net/software/vlc-3.0.18-win64.exe -OutFile c:\edrm\vlc-3.0.18-win64.exe
-Start-Process -FilePath c:\edrm\vlc-3.0.18-win64.exe -ArgumentList @('/L=1033', '/S') -Wait
+Invoke-WebRequest -Uri https://strgedrm.blob.core.windows.net/software/vlc-3.0.18-win64.exe -OutFile c:\edrm\vlc-3.0.20-win64.exe
+Start-Process -FilePath c:\edrm\vlc-3.0.20-win64.exe -ArgumentList @('/L=1033', '/S') -Wait
