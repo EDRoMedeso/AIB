aefa7a7a5eb37188fe344c1ba985bb5ecd1a3e4c
Love Håkansson <love.hakansson@edrmedeso.com>
2024-05-17 13:42:07 +0200

    Create RescaleCLI.ps1

---
+++ RescaleCLI.ps1
@@ -0,0 +1,7 @@
+<#
+Script to download and extract the Rescale CLI
+Love Håkansson 2024-05-17
+#>
+
+Invoke-WebRequest -Uri https://strgedrm.blob.core.windows.net/software/rescale-cli_windows-x64_1_1_224-21e5194057.zip -OutFile c:\edrm\rescale.zip
+Expand-Archive -Path c:\edrm\rescale.zip -DestinationPath 'C:\RescaleCLI' -Force
