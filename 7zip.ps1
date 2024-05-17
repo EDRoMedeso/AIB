82ebf60d3355b6d56887610500c7043e2782d5e9
Love Håkansson <love.hakansson@edrmedeso.com>
2024-05-17 12:52:06 +0200

    Updated to v.24.05.

--- 7zip.ps1
+++ 7zip.ps1
@@ -1,7 +1,8 @@
 <#
 Script to install 7zip
 Love Håkansson 2023-09-20
+Updated 2024-05-17 v.24.05
 #>
 
-Invoke-WebRequest -Uri https://strgedrm.blob.core.windows.net/software/7z2301-x64.msi -OutFile c:\edrm\7zip.msi
+Invoke-WebRequest -Uri https://strgedrm.blob.core.windows.net/software/7z2405-x64.msi -OutFile c:\edrm\7zip.msi
 Start-Process -FilePath msiexec -ArgumentList @('/i', 'c:\\edrm\\7zip.msi', '/qn', 'INSTALLDIR="C:\\Program Files\\7-Zip"') -Wait
