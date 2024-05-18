04b3f95c0ddb8f2beea73301a6eae0d521de4d18
Love Håkansson <love.hakansson@edrmedeso.com>
2024-05-18 17:00:24 +0200

    Changing installation directory to Program Files

--- RescaleCLI.ps1
+++ RescaleCLI.ps1
@@ -4,4 +4,4 @@ Love Håkansson 2024-05-17
 #>
 
 Invoke-WebRequest -Uri https://strgedrm.blob.core.windows.net/software/rescale-cli_windows-x64_1_1_224-21e5194057.zip -OutFile c:\edrm\rescale.zip
-Expand-Archive -Path c:\edrm\rescale.zip -DestinationPath 'C:\RescaleCLI' -Force
+Expand-Archive -Path c:\edrm\rescale.zip -DestinationPath 'C:\Program Files\RescaleCLI' -Force
