ad527409e9d991fe321b2a4cf423f93adae79968
Love Håkansson <love.hakansson@edrmedeso.com>
2024-05-22 23:00:51 +0200

    Added "'"

--- RevokeAuthenticatedUsers.ps1
+++ RevokeAuthenticatedUsers.ps1
@@ -4,5 +4,5 @@ and adding Modify permissions on c:\temp
 Love Håkansson 2024-05-22
 #>
 
-cmd.exe /c 'icacls c:\\ /remove:g "Authenticated Users"
+cmd.exe /c 'icacls c:\\ /remove:g "Authenticated Users"'
 cmd.exe /c 'icacls c:\\temp /grant:r "Authenticated Users":(CI)(OI)(M)'
