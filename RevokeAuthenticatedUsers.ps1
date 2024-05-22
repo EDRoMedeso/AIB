c498234cf0f4b9ef0f8e3e9ae663b3867b7417f4
Love Håkansson <love.hakansson@edrmedeso.com>
2024-05-22 20:25:32 +0200

    Adding a short description

--- RevokeAuthenticatedUsers.ps1
+++ RevokeAuthenticatedUsers.ps1
@@ -1,2 +1,8 @@
+<#
+Script to remove the NTFS permissions for Authenticated Users on C:\
+and adding Modify permissions on c:\temp
+Love Håkansson 2024-05-22
+#>
+
 cmd.exe /c 'icacls c:\\ /remove:g "Authenticated Users"
 cmd.exe /c 'icacls c:\\temp /grant:r "Authenticated Users":(CI)(OI)(M)'
