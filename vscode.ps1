eeee36410d88e4c2891395b8d25efa3a9ccbe123
Love Håkansson <love.hakansson@edrmedeso.com>
2025-08-07 08:34:37 +0200

    Update vscode.ps1 to v.1.102.3

--- vscode.ps1
+++ vscode.ps1
@@ -1,7 +1,7 @@
 <#
 Script to install VS Code
-Love Håkansson 2023-10-10, 2024-01-20, 2024-09-10, 2025-01-03
+Love Håkansson 2023-10-10, 2024-01-20, 2024-09-10, 2025-01-03, 2025-08-07
 #>
 
-Invoke-WebRequest -Uri https://strgedrm.blob.core.windows.net/software/VSCodeSetup-x64-1.96.2.exe -OutFile c:\edrm\VSCodeSetup-x64-1.96.2.exe
-Start-Process -FilePath c:\edrm\VSCodeSetup-x64-1.96.2.exe -ArgumentList @('/SP-', '/VERYSILENT', '/NORESTART', '/NOCANCEL', '/MERGETASKS=!runcode') -Wait
+Invoke-WebRequest -Uri https://strgedrm.blob.core.windows.net/software/VSCodeSetup-x64-1.102.3.exe -OutFile c:\edrm\VSCodeSetup-x64-1.102.3.exe
+Start-Process -FilePath c:\edrm\VSCodeSetup-x64-1.102.3.exe -ArgumentList @('/SP-', '/VERYSILENT', '/NORESTART', '/NOCANCEL', '/MERGETASKS=!runcode') -Wait
