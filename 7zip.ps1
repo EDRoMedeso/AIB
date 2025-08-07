b5088cf644f4c01f16d1aee53ada1f0a65e89ecb
Love Håkansson <love.hakansson@edrmedeso.com>
2025-08-07 08:00:40 +0200

    Update 7zip.ps1 to v.25.01

--- 7zip.ps1
+++ 7zip.ps1
@@ -4,7 +4,8 @@ Love Håkansson 2023-09-20
 Updated 2024-05-17 v.24.05
 Updated 2024-09-10 v.24.08
 Updated 2025-01-03 v.24.09
+Updated 2025-08-07 v.25.01
 #>
 
-Invoke-WebRequest -Uri https://strgedrm.blob.core.windows.net/software/7z2409-x64.msi -OutFile c:\edrm\7zip.msi
+Invoke-WebRequest -Uri https://strgedrm.blob.core.windows.net/software/7z2501-x64.msi -OutFile c:\edrm\7zip.msi
 Start-Process -FilePath msiexec -ArgumentList @('/i', 'c:\\edrm\\7zip.msi', '/qn', 'INSTALLDIR="C:\\Program Files\\7-Zip"') -Wait
