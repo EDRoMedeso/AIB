2a843e99426d2769c98b362b48ec28fc0aad8a4e
Love Håkansson <love.hakansson@edrmedeso.com>
2026-05-07 11:38:47 +0200

    Change 7-Zip download link to version 26.01
    Updated the download link for 7-Zip to version 26.01.

--- 7zip.ps1
+++ 7zip.ps1
@@ -5,7 +5,8 @@ Updated 2024-05-17 v.24.05
 Updated 2024-09-10 v.24.08
 Updated 2025-01-03 v.24.09
 Updated 2025-08-07 v.25.01
+Updated 2026-05-07 v.26.01
 #>
 
-Invoke-WebRequest -Uri https://strgedrm.blob.core.windows.net/software/7z2501-x64.msi -OutFile c:\edrm\7zip.msi
+Invoke-WebRequest -Uri https://strgedrm.blob.core.windows.net/software/7z2601-x64.msi -OutFile c:\edrm\7zip.msi
 Start-Process -FilePath msiexec -ArgumentList @('/i', 'c:\\edrm\\7zip.msi', '/qn', 'INSTALLDIR="C:\\Program Files\\7-Zip"') -Wait
